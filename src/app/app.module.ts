982dc3a1d9841d0bcb9b7eeb7e9b21b565fe67ee
Kevin Wu <kevinnwu220@gmail.com>
2018-12-02 22:02:57 -0800

    3rd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardsComponent } from './boards/boards.component';
+import { BoardsDetailComponent } from './boards-detail/boards-detail.component';
+import { BoardsCreateComponent } from './boards-create/boards-create.component';
+import { BoardsEditComponent } from './boards-edit/boards-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardsComponent,
+    BoardsDetailComponent,
+    BoardsCreateComponent,
+    BoardsEditComponent
   ],
   imports: [
     BrowserModule
